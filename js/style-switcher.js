5773a98b44bf0b4de46e45951020ab82db542067
Daniels35 <danielstiven35@gmail.com>
2023-12-18 03:41:18 -0500

    Persistencia color y modo oscuro

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -10,35 +10,50 @@ window.addEventListener("scroll", () => {
         document.querySelector(".style-switcher").classList.remove("open");
     }
 })
-/* ========================== theme colors =========================== */
+// ========================== theme colors ===========================
 const alternateStyles = document.querySelectorAll(".alternate-style");
-function setActiveStyle(color)
-{
-    alternateStyles.forEach((style) => {
-        if(color === style.getAttribute("title"))
-        {
-            style.removeAttribute("disabled");
-        }
-        else
-        {
-            style.setAttribute("disabled","true");
-        }
-    })
+
+const savedColor = localStorage.getItem('themeColor');
+
+function setActiveStyle(color) {
+  alternateStyles.forEach((style) => {
+    if (color === style.getAttribute("title")) {
+      style.removeAttribute("disabled");
+    } else {
+      style.setAttribute("disabled", "true");
+    }
+  });
+  localStorage.setItem('themeColor', color);
 }
+const defaultColor = savedColor;
+setActiveStyle(defaultColor);
+
 /* ========================== theme light and dark mode =========================== */
 const dayNight = document.querySelector(".day-night");
-dayNight.addEventListener("click", () => {
-    dayNight.querySelector("i").classList.toggle("fa-sun");
-    dayNight.querySelector("i").classList.toggle("fa-moon");
-    document.body.classList.toggle("dark");
-})
+
+const currentMode = localStorage.getItem('themeMode');
+
+function toggleMode() {
+  document.body.classList.toggle("dark");
+
+  if (document.body.classList.contains("dark")) {
+    dayNight.querySelector("i").classList.add("fa-sun");
+    dayNight.querySelector("i").classList.remove("fa-moon");
+  } else {
+    dayNight.querySelector("i").classList.remove("fa-sun");
+    dayNight.querySelector("i").classList.add("fa-moon");
+  }
+
+  const currentMode = document.body.classList.contains("dark") ? 'dark' : 'light';
+  localStorage.setItem('themeMode', currentMode);
+}
+
 window.addEventListener("load", () => {
-    if(document.body.classList.contains("dark"))
-    {
-        dayNight.querySelector("i").classList.add("fa-sun");
-    }
-    else
-    {
-        dayNight.querySelector("i").classList.add("fa-moon");
-    }
-})
+  if (currentMode === 'dark') {
+    toggleMode();
+  } else {
+    dayNight.querySelector("i").classList.add("fa-moon");
+  }
+});
+
+dayNight.addEventListener("click", toggleMode);
